2f72dae09331d2270176b6c599a0bda32d7655a6
Vigitoreanec <chirik39@mail.ru>
2024-11-05 19:16:48 +0200

    Final изменения внесены, пример готов

--- components/Movie.jsx
+++ components/Movie.jsx
@@ -1,31 +1,53 @@
 import React from "react";
+import { useRouter } from "next/router";
+import style from "./movie.module.css";
 
 export function Movie({ selectedMovie }) {
     console.debug(selectedMovie, 'selected');
-    return <div className="movie-details" key={selectedMovie.id}>
-        <h2>{selectedMovie.Title}</h2>
-         <div className="movies">
-
-            <img src={selectedMovie.img} alt={selectedMovie.title} />
-            <div className="info">
-                <span className="title">
-                    <p>
-                        Title: {selectedMovie.Title}
+    const router = useRouter();
+    const url = '';
+    return <div className={style.movieDetails} key={selectedMovie.imdbID}>
+        <div className={style.movie}>
+            <img src={selectedMovie.Poster} alt={selectedMovie.Title} />
+            <div className={style.movieInfo}>
+                <h2 >{selectedMovie.Title}</h2>
+                <span className={style.movieYear}>
+                    <p >
+                        <b>Year: </b>{selectedMovie.Year}
                     </p>
                 </span>
-                <span className="year">
-                    <p>
-                        Year: {selectedMovie.Year}
-                    </p>
+                <span className={style.movieRuntime}>
+                    <p ><b>Runtime:</b> {selectedMovie.Runtime}</p>
                 </span>
-                <span className="rating">
-                    <p>
-                        Rating: {selectedMovie.imdbRating}
+                <span className={style.movieGenre}>
+                    <p ><b>Genre:</b> {selectedMovie.Genre}</p>
+                </span>
+                <span className={style.movieLanguage}>
+                    <p ><b>Language:</b> {selectedMovie.Language}</p>
+                    <p ><b>Country:</b> {selectedMovie.Country}</p>
+                </span>
+                <span className={style.movieAwards}>
+                    <p ><b>Awards:</b> {selectedMovie.Awards}</p>
+                </span>
+                <span className={style.movieActors}>
+                    <p ><b>Actors:</b> {selectedMovie.Actors}</p>
+                </span>
+                <span className={style.movieRating}>
+                    <p >
+                        <b>Rating:</b> {selectedMovie.imdbRating}
                     </p>
                 </span>
             </div>
+            <a href="/">
+                <button>
+                    ❌
+                </button>
+            </a>
         </div>
-        <button >Закрыть</button>
+        {/* <button className={style.button}
+            type="button" onClick={() => router.push('../')}>
+            Закрыть
+        </button> */}
     </div>
 
 
